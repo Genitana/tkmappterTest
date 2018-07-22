b2e3c43b921377bdb5cf9d7723edf9a8ebf9e5d0
robert <1426218266@qq.com>
2018-07-22 17:25:27 +0800

    add  stream  test

--- src/main/java/com/example/tkmappterTest/person/controller/PesonController.java
+++ src/main/java/com/example/tkmappterTest/person/controller/PesonController.java
@@ -1,27 +1,50 @@
 package com.example.tkmappterTest.person.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import com.example.tkmappterTest.person.dao.PersonDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tkmappterTest.person.entity.Person;
 import com.example.tkmappterTest.person.service.PersonService;
 import com.github.pagehelper.PageInfo;
+import tk.mybatis.mapper.entity.Condition;
 
 @RestController
 public class PesonController {
 
 	@Autowired
 	private PersonService personService ;
-	
+	@Autowired
+	private PersonDao personDao ;
+
 	@GetMapping("/person")
 	public ResponseEntity<?> getPerson(  int page,int size){
 		List<Person> pesonlist = personService.findPersons(page , size);
 		PageInfo<List<Person>> pageinfo = new PageInfo(pesonlist);
 		return new ResponseEntity<>(pageinfo, HttpStatus.OK);
 	}
-}
+
+	@PostMapping("/save")
+	public Person savePerson(@RequestBody Person person){
+		person.setUuid(UUID.randomUUID().toString());
+		personDao.insert(person);
+//		return new ResponseEntity<>("success",HttpStatus.OK);
+		return person;
+	}
+
+	@GetMapping("/person2")
+	public List<Person> getPerson(){
+		Condition condition = new Condition(Person.class);
+		condition.setOrderByClause("age desc");
+		condition.createCriteria().andEqualTo("sex",0);
+		return personDao.selectByExample(condition);
+	}
+ }
